dfb4e4b5b4c07ae2be1762a7d3ad6f1c0cf9cf03
Oussama El Akhiri <oussama.elakhiri@usms.ac.ma>
2024-10-23 17:01:00 +0100

    Setting up environment

---
+++ BookReadingTracker/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/client/**/*.html'],
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
